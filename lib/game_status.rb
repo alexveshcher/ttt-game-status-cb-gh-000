296d06f68980d6238c9edffd60366388eb991925
Oleksandr Veshcherovych <s.veshcherovych@gmail.com>
2018-11-19 18:25:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,6 +28,7 @@ def won?
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination # return the win_combination indexes that won.
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
     else
       false
     end
